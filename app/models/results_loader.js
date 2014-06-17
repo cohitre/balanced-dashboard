4b0129a1d6c65783749f0e902b4b009497f0bf82
cohitre <carlosrr@gmail.com>
2014-06-17 14:29:15 -0700

    Temporary results refactoring

---
+++ app/models/results_loader.js
@@ -0,0 +1,34 @@
+Balanced.ResultsLoader = Ember.Object.extend({
+	resultsUri: function() {
+		return this.get('path') + "?" + this.get('queryString');
+	}.property("path", "queryString"),
+
+	queryString: function() {
+		var filteringParams = this.get("queryStringArguments");
+		return $.map(filteringParams, function(v, k) {
+			return encodeURIComponent(k) + '=' + encodeURIComponent(v);
+		}).join('&');
+	}.property("queryStringArguments"),
+
+	results: function() {
+		var uri = this.get('resultsUri');
+		var type = this.get('resultsType');
+		return Balanced.SearchModelArray.newArrayLoadedFromUri(uri, type);
+	}.property("resultsUri", "resultsType")
+});
+
+Balanced.TransactionsResultsLoader = Balanced.ResultsLoader.extend({
+	resultsType: Balanced.Transaction,
+	path: "/transactions",
+
+	queryStringArguments: function() {
+		return {
+			limit: 50,
+			sort: "created_at,desc",
+			q: "",
+			sort: "created_at,desc",
+			"status[in]": "failed,succeeded,pending",
+			"type[in]": "debit,credit,hold,refund"
+		};
+	}.property(),
+});
